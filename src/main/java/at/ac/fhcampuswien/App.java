768ff693710970a1fd86935e75bbb29f8fa9ff1c
nicki <nicki.faustino@stud.fh-campuswien.ac.at>
2021-06-01 22:42:32 +0200

    Password Length

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,6 +1,13 @@
 package at.ac.fhcampuswien;
 
 public class App {
+    public boolean checkPassword(String password){
+        if(password.length()>28||password.length()<8){
+            return false;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
     }
