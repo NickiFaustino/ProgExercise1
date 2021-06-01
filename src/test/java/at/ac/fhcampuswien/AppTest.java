12bed9cc61b4efe29d1bace595e3a250e8f1f18f
nicki <nicki.faustino@stud.fh-campuswien.ac.at>
2021-06-01 23:38:56 +0200

    Letter and Number checking in apptest

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -14,7 +14,22 @@ public class AppTest {
     @Test
     public void checkPassword2(){
         App password = new App();
-        boolean answer=password.checkPassword("Ich");
+        boolean answer=password.checkPassword("Hallo");
         assertFalse(answer);
     }
+
+    @Test
+    public void checkPassword3(){
+        App password = new App();
+        boolean answer = password.checkPassword("asdasdasdasd");
+        assertFalse(answer);
+    }
+
+    @Test
+    public void checkPassword4(){
+        App password = new App();
+        boolean answer = password.checkPassword("123123123123");
+        assertFalse(answer);
+    }
+
 }
