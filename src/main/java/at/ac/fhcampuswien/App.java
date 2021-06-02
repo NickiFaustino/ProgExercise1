d83eccb552beb8853cbc769f7caba3952b409039
nicki <nicki.faustino@stud.fh-campuswien.ac.at>
2021-06-02 09:39:34 +0200

    Fixed length problem and added tests

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien;
 
 public class App {
+    int length=0;
     int letcount = 0;
     int numbcount = 0;
     int uppercount = 0;
@@ -9,39 +10,37 @@ public class App {
 
 
     public boolean checkPassword(String password){
-        if(password.length()>28 && password.length()<8) {
-            return false;
-        }
-        for(int j = 0; j <password.length(); j++){
-            char ch = password.charAt(j);
-            if (isUpper(ch)){
-                uppercount++;
-            }
-            else if (isLower(ch)){
-                lowercount++;
-            }
-            else if (isSpecial(ch)){
-                specialcount++;
-            }
-        }
-
+       // if(password.length()>28 && password.length()<8) {
+         //   return false;
+        //}
         for(int i = 0; i < password.length(); i++){
             char word =  password.charAt(i);
 
             if (isNumeric(word)){
                 numbcount++;
+                length++;
             }
-            else if(isLetter(word)){
+            if (isLetter(word)){
                 letcount++;
+                length++;
             }
-
+            if (isUpper(word)){
+                uppercount++;
+            }
+            if (isLower(word)){
+                lowercount++;
+            }
+            if (isSpecial(word)){
+                specialcount++;
+                length++;
+            }
         }
 
-        if (numbcount > 0 && letcount > 0 && uppercount > 0 && lowercount > 0 && specialcount >0){
+        if (numbcount > 0 && letcount > 0 && uppercount > 0 && lowercount > 0 && specialcount >0 && length > 8 && length < 25){
             return true;
 
-        //if (uppercount >0 && lowercount >0){
-          //  return true;
+            //if (uppercount >0 && lowercount >0){
+            //  return true;
         }else{
             return false;
         }
@@ -53,17 +52,16 @@ public class App {
         return (word >= '0' && word <= '9');
     }
     public static boolean isLetter(char word) {
-        word = Character.toUpperCase(word);
-        return (word >= 'A' && word <= 'Z');
+        return (word >= 'A' && word <= 'Z' || word >= 'a' && word <= 'z');
     }
-    public static boolean isUpper(char ch){
-        return (ch >=65&& ch <= 90);
+    public static boolean isUpper(char word){
+        return (word >=65&& word <= 90);
     }
-    public static boolean isLower(char ch){
-        return (ch >=97&& ch <= 122);
+    public static boolean isLower(char word){
+        return (word >=97&& word <= 122);
     }
-    public static boolean isSpecial(char ch){
-        return (ch =='('|| ch == ')'|| ch =='#' || ch == '$'|| ch =='?'||ch == '!'|| ch =='%' || ch =='/' || ch == '@');
+    public static boolean isSpecial(char word){
+        return (word =='('|| word == ')'|| word =='#'||  word == '$'|| word =='?'||word == '!'|| word =='%'|| word =='/'||  word == '@');
     }
 
     public static void main(String[] args) {
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,67 +1,94 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+    @DisplayName("Checks if password is valid when every requirement is met")
     @Test
     public void checkPassword(){
         App password = new App();
         boolean answer=password.checkPassword("Hallo1593?");
         assertTrue(answer);
     }
-
+    @DisplayName("Checks if password is invalid because it does not have enough characters but meets all requirements")
     @Test
     public void checkPassword2(){
         App password = new App();
-        boolean answer=password.checkPassword("Hallo#");
+        boolean answer=password.checkPassword("Hallo#2");
         assertFalse(answer);
     }
-
+    @DisplayName("Checks if password is invalid because it has too many characters but meets all requirements")
     @Test
     public void checkPassword3(){
         App password = new App();
-        boolean answer = password.checkPassword("asdasdasdasd");
+        boolean answer=password.checkPassword("Hallo#2aoiaAiu3()???$$$!!%");
         assertFalse(answer);
     }
-
+    @DisplayName("Checks if the password is invalid because it has no numbers")
     @Test
     public void checkPassword4(){
         App password = new App();
-        boolean answer = password.checkPassword("123123123123");
+        boolean answer = password.checkPassword("Asdasdasdasd#");
         assertFalse(answer);
     }
+    @DisplayName("Checks if the password is invalid because it has no letters --> No uppercase or lowercase")
     @Test
     public void checkPassword5(){
         App password = new App();
-        boolean answer = password.checkPassword("hallo1234");
+        boolean answer = password.checkPassword("159368#???");
         assertFalse(answer);
     }
+    @DisplayName("Checks if the password is invalid because it has no uppercase")
     @Test
     public void checkPassword6(){
         App password = new App();
-        boolean answer = password.checkPassword("HALLO1234");
+        boolean answer = password.checkPassword("hallo1234#");
         assertFalse(answer);
     }
+    @DisplayName("Checks if the password is invalid because it has no lowercase")
     @Test
     public void checkPassword7(){
+        App password = new App();
+        boolean answer = password.checkPassword("HALLO1234#");
+        assertFalse(answer);
+    }
+    @DisplayName("Checks if the password is valid because it meets all requirements")
+    @Test
+    public void checkPassword8(){
         App password = new App();
         boolean answer = password.checkPassword("Hallo123434#");
         assertTrue(answer);
     }
+    @DisplayName("Checks if the password is invalid because it has a different special character")
     @Test
-    public void checkPassword8(){
+    public void checkPassword9(){
         App password = new App();
         boolean answer=password.checkPassword("Hallo1593*");
         assertFalse(answer);
     }
+    @DisplayName("Checks if the password is valid because it meets all requirements specifically test if uppercase in the middle stil leads to valid")
     @Test
-    public void checkPassword9(){
+    public void checkPassword10(){
         App password = new App();
         boolean answer = password.checkPassword("HalloAA123434#");
         assertTrue(answer);
     }
-
+    @DisplayName("Checks if password is invalid because it has no special characters")
+    @Test
+    public void checkPassword11(){
+        App password = new App();
+        boolean answer = password.checkPassword("HalloAA123434");
+        assertFalse(answer);
+    }
+    @DisplayName("Checks if password is invalid because it only has special characters")
+    @Test
+    public void checkPassword12(){
+        App password = new App();
+        boolean answer = password.checkPassword("???????????????????????");
+        assertFalse(answer);
+    }
 
 
 }
